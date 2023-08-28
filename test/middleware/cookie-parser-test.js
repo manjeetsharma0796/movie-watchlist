173c6a3ded06b370ec0d614cbc1b17a4b4b96ebe
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-08-29 01:42:47 +0530

    add: test coverage for cookie-parse

---
+++ test/middleware/cookie-parser-test.js
@@ -0,0 +1,35 @@
+const { describe, it } = require("node:test");
+const assert = require("assert");
+const { parseCookie } = require("../../src/middleware/cookie-parser");
+
+describe("cookieParser", () => {
+  it("should be empty if no cookie passed", () => {
+    const emptyFn = () => {};
+
+    const request = { headers: {} };
+
+    parseCookie(request, emptyFn, emptyFn);
+    assert.deepStrictEqual(request.cookies, {});
+  });
+
+  it("should parse raw cookie", () => {
+    const emptyFn = () => {};
+    const request = { headers: { cookie: "username=Jerry" } };
+
+    parseCookie(request, emptyFn, emptyFn);
+
+    assert.deepStrictEqual(request.cookies, { username: "Jerry" });
+  });
+
+  it("should parse multiple raw cookie", () => {
+    const emptyFn = () => {};
+    const request = { headers: { cookie: "username=Jerry; initials=J" } };
+
+    parseCookie(request, emptyFn, emptyFn);
+
+    assert.deepStrictEqual(request.cookies, {
+      username: "Jerry",
+      initials: "J",
+    });
+  });
+});
